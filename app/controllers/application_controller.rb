3eaf772ebba2aae24468c23603eb5dc053833412
rsetoguchi <rsetoguchi@netprotections.co.jp>
2024-09-02 01:08:40 +0900

    rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # before_action do
   #   redirect_to access_denied_path if params[:token].blank?
   # end
+
+  # rescue_from LoginFailed, with: :login_failed
+
+  # def login_failed
+  #   render template: 'shared/login_failed', status: 401
+  # end
 end
