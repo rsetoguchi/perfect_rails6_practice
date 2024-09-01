52f6a6a35c4f5f0a769ce8987cae33354100cdd4
rsetoguchi <rsetoguchi@netprotections.co.jp>
2024-09-01 21:47:54 +0900

    resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
 
   resources :tasks
   resources :publishers
+  resource :prifile, only: %i{show edit update} # onlyを付けることで、指定したルーティングのみ生成するよう制限
 
   # resources :publishers do
   #   resources :books
