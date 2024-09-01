fff1dc389309b6bbacf818d8e43d9c226b43a0e9
rsetoguchi <rsetoguchi@netprotections.co.jp>
2024-09-01 21:29:26 +0900

    resourcesルーティングの拡張（確認後コメントアウト）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
+
   resources :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :publishers
+
+  # resources :publishers do
+  #   resources :books
+  #
+  #   # memberを利用すると、「/publishers/:id/detail」のように、個別のリソースに対してアクションを設定する
+  #   member do
+  #     get 'detail'
+  #   end
+  #
+  #   # collectionを利用すると、「/publishers/search」のように、全体のリソースに対するアクションを設定する
+  #   collection do
+  #     get 'search'
+  #   end
+  # end
 end
